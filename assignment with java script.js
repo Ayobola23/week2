b9bf185e9f6224d975447518b61f9fe3c4bdaa8c
Ayobola23 <127272170+Ayobola23@users.noreply.github.com>
2023-03-27 16:53:13 +0100

    Add files via upload

---
+++ assignment with java script.js
@@ -0,0 +1,7 @@
+alert("This webpage is better viewed on a PC");
+
+let loginForm = document.getElementById("loginForm");
+
+function mybutton() {
+    alert("YOU HAVE SUCCESFULLY SUBMITED TO SIDE HUSTLE.COM");
+  }
